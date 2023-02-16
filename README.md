9a61bfa803842cedffbddf8555d7c5911a1c1c5e
DN4Davi <davizinhonascimento@gmail.com>
2023-02-16 11:26:08 -0300

    add second test and update requirements in README.md

--- README.md
+++ README.md
@@ -11,4 +11,11 @@ Inspirado em um projeto que realizei no módulo de fundamentos da trybe, porém 
 - Ambiente de desenvolvimento com Vite;
 
 ## Requisitos do projeto
-- [x] Coming Soon
+- [ ] Adicionar todos os testes;
+- [ ] Adicionar o título "Pixels Art" à página;
+- [ ] Adicionar uma paleta de cores com 6 cores distintas;
+- [ ] Adicionar um input do tipo color que altere a cor selecionada;
+- [ ] Salvar as cores geradas no LocalStorage e recuperá-las ao recarrecar a página;
+- [ ] Adicionar um grid com 25 pixels na cor branca;
+- [ ] Pintar os pixels com a cor selecionada ao clicá-los;
+- [ ] Adicionar um botão que limpa o quadro;
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,31 @@
 describe("Adicionar o título 'Pixels Art' à página", () => {
   beforeEach(() => cy.visit("http://localhost:5173/pixels-art/"));
 
-  it("Verifica se existe um <h1> com [data-cy='title']", () => {
-    cy.get("h1[data-cy='title']").should('exist');
+  it("Verifica se existe um elemento com [data-cy='title']", () => {
+    cy.get("[data-cy='title']").should('exist');
   });
 
-  it("Verifica se o referido h1 possui o texto 'Pixels Art'", () => {
-    cy.get("h1[data-cy='title']").contains("Pixels Art");
+  it("Verifica se o referido elemento é um <h1> possui o texto 'Pixels Art'", () => {
+    cy.get("[data-cy='title']").contains('h1',"Pixels Art");
   });
 
+});
+
+describe("Adicionar uma paleta de cores com 6 cores distintas", () => {
+  beforeEach(() => cy.visit("http://localhost:5173/pixels-art/"));
+
+  it("Verifica se existe um elemento com [data-cy='colors-palette']", () => {
+    cy.get('[data-cy="colors-palette"').should("exist");
+  });
+
+  it("Verifica se existem 6 elementos com [data-cy='color'] dentro da paleta de cores", () => {
+    cy.get('[data-cy="colors-palette"')
+    .get("[data-cy='color']")
+    .should('have.length', 6)
+    .then((colors) => {
+      const colorsArray = Array.from(colors)
+      .map((color) => getComputedStyle(color).backgroundColor)
+      expect(Array.from(new Set(colorsArray)).length).to.equal(colorsArray.length)
+    });
+  });
 });
